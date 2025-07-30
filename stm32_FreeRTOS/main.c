05dda6f19fbaa4635d73e891e78a8459c598fe2c
LinTinWei <yam22975918@gmail.com>
2025-07-30 16:53:44 +0800

    FreeRTOS tasks communicate with Queue.

--- stm32_FreeRTOS/main.c
+++ stm32_FreeRTOS/main.c
@@ -1,26 +1,30 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "uart.h"
-#include "led.h"
+#include "queue.h"
 
+QueueHandle_t xQueue;
 
-// task.c 中宣告的任務函數
-void vTaskLED(void *pvParameters);
-void vTaskUART(void *pvParameters);
-
+void vTaskProducer(void *pvParameters);
+void vTaskConsumer(void *pvParameters);
 int main(void) {
     	uart_init();
-	led_init();	
-	uart_print("FreeRTOS STM32 Starting...\r\n");
+	uart_print("FreeRTOS Queue Demo...\r\n");
 
-	if (xTaskCreate(vTaskLED, "LED", 128, NULL, 1, NULL) != pdPASS)
-    		uart_print("Task LED creation failed!\r\n");
-	if (xTaskCreate(vTaskUART, "UART", 128, NULL, 2, NULL) != pdPASS)
-    		uart_print("Task UART creation failed!\r\n");
+	// 建立 Queue: 可存 10 筆 uint16_t
+	xQueue = xQueueCreate(10, sizeof(uint16_t));
 
-	uart_print("Starting scheduler...\r\n");
+	if (xQueue == NULL) {
+		uart_print("Queue create Failed!");
+		while(1);
+	}
+
+	xTaskCreate(vTaskProducer, "Producer", 128, NULL, 1, NULL);
+	xTaskCreate(vTaskConsumer, "Consumer", 128, NULL, 1, NULL);
+
+
 	vTaskStartScheduler();
-	uart_print("Scheduler failed to start!\r\n");  // 若看到這句就代表 FreeRTOS 出錯
+	while(1);
 
 	return 0;
 }
